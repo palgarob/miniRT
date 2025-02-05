23a06e910d4a64b2998cd89556946745da1865cc
Pedro Palomares <pedpalgarob@gmail.com>
2025-02-05 16:11:04 +0100

    something weird happens when the plane is not perpendicular to the camera. the ray's direction denormalizes despite there not being an scale mat present

--- src/ray_stuff/pl_is_intersected.c
+++ src/ray_stuff/pl_is_intersected.c
@@ -6,7 +6,7 @@
 /*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 11:11:45 by pepaloma          #+#    #+#             */
-/*   Updated: 2025/02/05 14:45:07 by pepaloma         ###   ########.fr       */
+/*   Updated: 2025/02/05 16:09:50 by pepaloma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 bool	pl_is_intersected(t_ray *r, double t[2])
 {
+	printf("%f ", r->direction.z);
 	if (fpn_compare(r->direction.z, 0) == EQUAL)
 		return (false);
 	t[0] = -r->origin.z / r->direction.z;
